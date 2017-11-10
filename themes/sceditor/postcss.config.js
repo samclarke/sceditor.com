29d3dc7aea04e21cca71228eb075bbacf5fae71b
Sam Clarke <sam@samclarke.com>
2017-11-10 13:01:47 +0000

    Add autoprefixer to postcss

--- themes/sceditor/postcss.config.js
+++ themes/sceditor/postcss.config.js
@@ -4,8 +4,14 @@ module.exports = {
         require('postcss-import')({
             root: './static/css/'
         }),
+        require('autoprefixer')({
+            browsers: [
+                'last 4 versions',
+                'ie 10'
+            ]
+        }),
         require('cssnano')({
             preset: 'default',
-        }),
+        })
     ],
 };
